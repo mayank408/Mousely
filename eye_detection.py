fe54cc8c62e3cffef5ec21bf536eab2c9962c10e
Mayank <mayank.trp48@gmail.com>
2017-10-24 23:26:51 +0530

    minor improvements

--- eye_detection.py
+++ eye_detection.py
@@ -277,6 +277,8 @@ while True:
         cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
+        break
+
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
